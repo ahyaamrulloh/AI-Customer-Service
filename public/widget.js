c1e30a2c3f1215fa9cf2d8c43158b9d58d553fd6
ahyaamrulloh <ahyaamrulloh2@gmail.com>
2026-05-13 20:13:24 +0700

    update widget icons

--- public/widget.js
+++ public/widget.js
@@ -78,8 +78,8 @@
   // Membuat tombol ☕ floating
   const btn = document.createElement("button");
   btn.id = "kn-widget-btn";
-  btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white">
-      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+  btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="white">
+      <path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z"/>
     </svg>`;
   btn.setAttribute("aria-label", "Buka chat Coffee Nusantara");
   document.body.appendChild(btn);
